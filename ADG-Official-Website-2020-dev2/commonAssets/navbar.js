451ee63f28737baa8b761e76996db5e56e47c7a3
shubhankarshankar <shubhankarshankar@gmail.com>
2020-04-04 17:23:33 +0530

    new deployment

--- ADG-Official-Website-2020-dev2/commonAssets/navbar.js
+++
@@ -1,7 +0,0 @@
-function classToggle() {
-  const navs = document.querySelectorAll(".nav-links");
-
-  navs.forEach(nav => nav.classList.toggle("Navbar__ToggleShow"));
-}
-
-document.querySelector(".burger").addEventListener("click", classToggle);
