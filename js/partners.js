61508dcf5116ad68e3d5f7d2c05fb3d7c49eeac3
shubhankarshankar <shubhankarshankar@gmail.com>
2020-04-05 03:25:23 +0530

    Final Parterns Page

--- js/partners.js
+++
@@ -1,8 +0,0 @@
-var prevScrollpos = window.pageYOffset;
-
-window.scroll = function () {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("down-arrow").classList.toggle("hidden");
-  }
-};
