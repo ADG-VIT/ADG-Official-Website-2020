75699aabd5d335fd392e95b424f8613e7b052215
shubhankarshankar <shubhankarshankar@gmail.com>
2020-04-06 21:02:05 +0530

    Final Footer

--- commonAssets/js/navbar.js
+++
@@ -1,8 +0,0 @@
-var burger = document.getElementById("burger");
-var menu = document.getElementById("menu");
-var mobileAdgLogo = document.getElementById("mobile-adg-logo");
-
-burger.addEventListener("click", () => {
-  menu.classList.toggle("hidden");
-  mobileAdgLogo.classList.toggle("hidden");
-});
