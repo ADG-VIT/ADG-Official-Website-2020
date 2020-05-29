3ce8eacdf0668c87d59f69762425e3f4be28708c
divesharora <diveshngp@gmail.com>
2020-05-29 16:51:28 +0530

    Cards and footer

---
+++ commonAssets/js/preloader.js
@@ -0,0 +1,8 @@
+var overlay=document.getElementById('overlay');
+var main=document.getElementById('main');
+window.addEventListener('load', function(){
+    overlay.style.display='none';
+
+    main.style.display='initial';
+    
+});
